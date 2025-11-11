c6b2999d0ebdc827e23818f28f2e4b0be2892dba
Syed Safi Ali <safialicr@gmail.com>
2025-11-11 09:46:43 +0100

    Added the xcode project into the folder

---
+++ Translify/Translify/TranslifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslifyApp.swift
+//  Translify
+//
+//  Created by Syed Safi Ali on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
